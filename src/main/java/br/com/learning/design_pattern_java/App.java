87b5270be7cb60d6c6d38a8958db2f33c14f05ff
brucilin.de.gouveia <brucilin.de.gouveia@accenture.com>
2019-11-16 16:17:53 -0300

    Criando projeto java maven

---
+++ src/main/java/br/com/learning/design_pattern_java/App.java
@@ -0,0 +1,13 @@
+package br.com.learning.design_pattern_java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
